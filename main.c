4fc7daff5095277c2eed519b9ff1f6112ac95b4f
SomeUnknownNickname <vladcrave@gmail.com>
2025-06-24 00:18:23 +0300

    Add random room events and gold collection
    - Implemented playRandomEvent() with randomized effects on the player
    - Added ability to collect gold found in the room
    - Extended in-game menu with options for event handling and looting
    - Basic resource management and consequences introduced

--- main.c
+++ main.c
@@ -53,6 +53,68 @@ void loadGame(Player* player, char* playerName) {
     }
 }
 
+Room* createWorldMap() {
+    Room* rooms = malloc(sizeof(Room) * MAX_ROOMS);
+
+    for (int i = 0; i < MAX_ROOMS; i++) {
+        snprintf(rooms[i].description, sizeof(rooms[i].description), "You are in room #%d.", i + 1);
+        rooms[i].hasRandomEvent = rand() % 2;
+        rooms[i].goldAmount = rand() % 20;
+        rooms[i].hasNPC = rand() % 2;
+        rooms[i].hasTrader = (rand() % 4 == 0);
+        rooms[i].hasBoss = 0;
+        rooms[i].bossHealth = 0;
+    }
+
+    rooms[MAX_ROOMS - 1].hasBoss = 1;
+    rooms[MAX_ROOMS - 1].bossHealth = 100;
+    snprintf(rooms[MAX_ROOMS - 1].description, sizeof(rooms[MAX_ROOMS - 1].description), 
+             "This is the final room. A powerful boss awaits!");
+
+    return rooms;
+}
+
+void displayRoom(Room* rooms, int roomIndex, Player* player) {
+    Room room = rooms[roomIndex];
+    printf("\n\033[32m%s\033[0m\n", room.description);
+
+    if (room.goldAmount > 0)
+        printf("You see some gold here: \033[33m%d\033[0m coins.\n", room.goldAmount);
+    if (room.hasNPC)
+        printf("There is someone here who wants to talk to you.\n");
+    if (room.hasTrader)
+        printf("A trader offers to sell you gear.\n");
+    if (room.hasBoss)
+        printf("A terrifying boss stands before you!\n");
+
+    printf("Your stats: ❤️ %d | 💰 %d | 🗡️ %s | 🛡️ %s\n", 
+           player->health, player->gold,
+           player->hasSword ? "Yes" : "No",
+           player->hasArmor ? "Yes" : "No");
+}
+
+void playRandomEvent(Player* player) {
+    int event = rand() % 3;
+    switch (event) {
+        case 0:
+            printf("You fell into a trap and lost 10 HP!\n");
+            player->health -= 10;
+            break;
+        case 1:
+            printf("You found a healing potion and recovered 15 HP!\n");
+            player->health += 15;
+            break;
+        case 2:
+            printf("You encountered a thief and lost 5 gold!\n");
+            if (player->gold >= 5) {
+                player->gold -= 5;
+            } else {
+                player->gold = 0;
+            }
+            break;
+    }
+}
+
 int main(int argc, char *argv[]) {
     char playerName[MAX_NAME_LEN] = "";
 
@@ -85,70 +147,51 @@ int main(int argc, char *argv[]) {
     }
 
     Room* worldMap = createWorldMap();
+
     int gameRunning = 1;
     while (gameRunning) {
         displayRoom(worldMap, player.currentRoom, &player);
+
+        if (worldMap[player.currentRoom].hasRandomEvent) {
+            playRandomEvent(&player);
+            worldMap[player.currentRoom].hasRandomEvent = 0;
+        }
+
+        if (player.health <= 0) {
+            printf("You have died. Game over.\n");
+            break;
+        }
+
         printf("\n\033[36m[1] Next Room\n");
-        printf("[2] Save Game\n");
-        printf("[3] Quit\033[0m\n");
+        if (worldMap[player.currentRoom].goldAmount > 0)
+            printf("[2] Collect Gold\n");
+        printf("[3] Save Game\n");
+        printf("[4] Quit\033[0m\n");
 
         int choice;
         scanf("%d", &choice);
 
-    switch (choice) {
-        case 1:
-            player.currentRoom = (player.currentRoom + 1) % MAX_ROOMS;
-            break;
-        case 2:
-            saveGame(&player);
-            break;
-        case 3:
-            gameRunning = 0;
-            break;
-        default:
-            printf("Invalid option.\n");
+        switch (choice) {
+            case 1:
+                player.currentRoom = (player.currentRoom + 1) % MAX_ROOMS;
+                break;
+            case 2:
+                if (worldMap[player.currentRoom].goldAmount > 0) {
+                    printf("You collected %d gold!\n", worldMap[player.currentRoom].goldAmount);
+                    player.gold += worldMap[player.currentRoom].goldAmount;
+                    worldMap[player.currentRoom].goldAmount = 0;
+                } else {
+                    printf("There's no gold here.\n");
+                }
+                break;
+            case 3:
+                saveGame(&player);
+                break;
+            case 4:
+                gameRunning = 0;
+                break;
+            default:
+                printf("Invalid option.\n");
+        }
     }
 }
-
-}
-
-Room* createWorldMap() {
-    Room* rooms = malloc(sizeof(Room) * MAX_ROOMS);
-
-    for (int i = 0; i < MAX_ROOMS; i++) {
-        snprintf(rooms[i].description, sizeof(rooms[i].description), "You are in room #%d.", i + 1);
-        rooms[i].hasRandomEvent = rand() % 2;
-        rooms[i].goldAmount = rand() % 20;
-        rooms[i].hasNPC = rand() % 2;
-        rooms[i].hasTrader = (rand() % 4 == 0);
-        rooms[i].hasBoss = 0;
-        rooms[i].bossHealth = 0;
-    }
-
-    // Last room has the boss
-    rooms[MAX_ROOMS - 1].hasBoss = 1;
-    rooms[MAX_ROOMS - 1].bossHealth = 100;
-    snprintf(rooms[MAX_ROOMS - 1].description, sizeof(rooms[MAX_ROOMS - 1].description), 
-             "This is the final room. A powerful boss awaits!");
-
-    return rooms;
-}
-void displayRoom(Room* rooms, int roomIndex, Player* player) {
-    Room room = rooms[roomIndex];
-    printf("\n\033[32m%s\033[0m\n", room.description);
-
-    if (room.goldAmount > 0)
-        printf("You see some gold here: \033[33m%d\033[0m coins.\n", room.goldAmount);
-    if (room.hasNPC)
-        printf("There is someone here who wants to talk to you.\n");
-    if (room.hasTrader)
-        printf("A trader offers to sell you gear.\n");
-    if (room.hasBoss)
-        printf("A terrifying boss stands before you!\n");
-
-    printf("Your stats: ❤️ %d | 💰 %d | 🗡️ %s | 🛡️ %s\n", 
-           player->health, player->gold,
-           player->hasSword ? "Yes" : "No",
-           player->hasArmor ? "Yes" : "No");
-}
-
